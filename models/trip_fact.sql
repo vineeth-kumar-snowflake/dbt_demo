0e0da81e45b2e91574b577d3b4bf0cc6ffaa2cb7
vineeth.snowflake2@gmail.com <vineeth.snowflake2@gmail.com>
2026-07-10 12:24:03 +0000

    correction to the data

--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -2,15 +2,15 @@ with trips as (
 
     select 
     RIDE_ID,
-    RIDEABLE_TYPE,
+    --RIDEABLE_TYPE,
     date(to_timestamp(started_at)) as trip_date,
     START_STATIO_ID as start_station_id, 
     end_station_id, 
     MEMBER_CSUAL as member_casual,
     timestampdiff(second, to_timestamp(started_at), to_timestamp(ended_at)) as trip_duration_seconds
 
-    from {{ source('demo', 'bike') }}
-    where ride_id != 'ride_id'
+    from {{ ref('stg_bike') }}
+    where ride_id != '"bike_id"' and ride_id != 'bike_id'
 
 )
 
